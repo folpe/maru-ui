6394ed634866d3008c08e0bb6079adcb34a762bb
Florent Pellegrin <florent.pellegrin@gmail.com>
2021-01-09 07:46:55 +0100

    Update prettierrc
    change file name and active prettier in vscode parameters

--- src/components/atoms/Button/Button.spec.js
+++ src/components/atoms/Button/Button.spec.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import Button from './Button'
 
 describe('<Button />', () => {
-
-  it('should otherwise match snapshot', ()=> {
-    const {container} = render(<Button>Valider</Button>)
+  it('should otherwise match snapshot', () => {
+    const { container } = render(<Button>Valider</Button>)
 
     expect(container).toMatchSnapshot()
   })

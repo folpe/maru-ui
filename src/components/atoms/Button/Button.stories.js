abf8713688858c8524e5cef21d8def0662842148
Florent Pellegrin <florent.pellegrin@gmail.com>
2021-01-09 08:10:57 +0100

    Parse file with eslint and prettier

--- src/components/atoms/Button/Button.stories.js
+++ src/components/atoms/Button/Button.stories.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from 'react'
 
-import Button from './Button';
+import Button from './Button'
 
 export default {
   title: 'Atoms/Button',
   component: Button,
-};
+}
 
-export const Basic = () => <Button onClick={() => console.log('ici')}>Coucou</Button>;
-export const WithFunc = () => <Button onClick={() => console.log('ici')}>Hello</Button>;
+export const Basic = () => (
+  <Button onClick={() => console.log('ici')}>Coucou</Button>
+)
+
+export const WithFunc = () => (
+  <Button onClick={() => console.log('ici')}>Hello</Button>
+)

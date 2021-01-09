ec1437930f7d78a0576d339596d3036772d9db12
Florent Pellegrin <florent.pellegrin@gmail.com>
2021-01-09 06:12:33 +0100

    Add button sample component

---
+++ src/components/atoms/Button/Button.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { node } from 'prop-types';
+
+const Button = ({children}, props) => {
+  return (
+    <button {...props}>
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  children: node.isRequired,
+};
+
+export default Button;

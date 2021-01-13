8f9c866d0ecf68a7d886144e06cd7e4195ec378e
Florent Pellegrin <florent.pellegrin@gmail.com>
2021-01-13 15:48:31 +0100

    WIP for build

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,19 +3,66 @@ const { merge } = require('webpack-merge')
 
 const parts = require('./webpack.config.parts')
 
-console.log(parts)
-
 const root = path.resolve(__dirname, '..')
 
 const CORE_CONFIG = merge([
   {
-    entry: './src/index.js',
+    entry: {
+      main: path.resolve(root, 'src'),
+    },
     output: {
       path: path.resolve(root, 'dist'),
       filename: '[name].js',
+      publicPath: '/',
     },
   },
-  parts.loadSass(),
+  parts.friendlyErrors(),
+  parts.babelize({ include: path.resolve(root, 'src') }),
+  parts.lintJS(),
+  parts.loadFonts(),
+  parts.loadImages({ include: path.resolve(root, 'src') }),
+  parts.copyStatic({
+    from: path.resolve(root, 'static'),
+    to: path.resolve(root, 'dist'),
+  }),
+  parts.generateSourceMaps('source-map'),
 ])
 
-module.exports = CORE_CONFIG
+const productionConfig = () =>
+  merge([
+    {
+      mode: 'production',
+      // Cette nouvelle série d’options de Webpack 4 remplace pas mal d’anciennes manips,
+      // notamment tout ce qui touche à `CommonsChunkPlugin`.
+      optimization: {
+        // Extraction à part de la *runtime* Webpack
+        runtimeChunk: true,
+        splitChunks: {
+          // Ensure all CSS are put into a single file by MiniCSSExtractPlugin
+          cacheGroups: {
+            styles: {
+              name: 'styles',
+              test: /\.css$/,
+              chunks: 'all',
+              enforce: true,
+            },
+          },
+          // Auto-splitting intelligent de tous les chunks (initiaux et asynchrones)
+          // (par défaut, ça ne fait que les asynchrones).
+          chunks: 'all',
+        },
+      },
+      output: { filename: '[name].[chunkhash:8].js' },
+    },
+    CORE_CONFIG,
+    parts.cleanDist(),
+    parts.extractCSS(),
+    parts.extractSass({ include: path.resolve(root, 'src') }),
+    parts.generateSourceMaps('source-map'),
+    parts.minifyAll(),
+    parts.optimizeImages(),
+  ])
+
+console.log(CORE_CONFIG)
+
+module.exports = productionConfig()
--- webpack/webpack.config.parts.js
+++ webpack/webpack.config.parts.js
@@ -1,7 +1,221 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+exports.babelize = ({ include, exclude = /node_modules/ } = {}) => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          include,
+          exclude,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: { presets: ['@babel/preset-env'] },
+            },
+          ],
+        },
+      ],
+    },
+  }
+}
+exports.friendlyErrors = ({
+  icon,
+  notify = false,
+  title = 'Webpack Build Error',
+} = {}) => {
+  let opts = {}
+  if (notify) {
+    const notifier = require('node-notifier')
+    opts = {
+      onErrors(severity, errors) {
+        if (severity !== 'error') {
+          return
+        }
+
+        const error = errors[0]
+        notifier.notify({
+          title,
+          message: `${severity} : ${error.name}`,
+          subtitle: error.file || '',
+          icon,
+        })
+      },
+    }
+  }
+
+  return {
+    plugins: [new FriendlyErrorsWebpackPlugin(opts)],
+  }
+}
+
+exports.errorOverlay = () => {
+  const ErrorOverlayPlugin = require('error-overlay-webpack-plugin')
+  const path = require('path')
+
+  return {
+    // eval-based source maps can't work with this overlay
+    devtool: 'cheap-module-source-map',
+    // We need absolute filenames for proper click-to-open-editor behavior
+    output: {
+      devtoolModuleFilenameTemplate(info) {
+        return path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')
+      },
+    },
+    plugins: [new ErrorOverlayPlugin()],
+  }
+}
+
+exports.copyStatic = ({ from, to }) => ({
+  plugins: [
+    // Copie / met à dispo en mémoire des fichiers statiques vers un
+    // chemin en sortie.  On aurait pu les coller direct dans `public/`,
+    // mais alors Webpack ne les aurait pas «détectés», et d’autres
+    // plugins ne les auraient pas pris en compte (tels que la gestion
+    // de l’*offline*, par exemple).
+    new CopyWebpackPlugin({ patterns: [{ from, to }] }),
+  ],
+})
+
+// Source Maps
+// -----------
+//
+// Webpack nous propose une bonne demi-douzaine de types de
+// [source maps](https://www.html5rocks.com/en/tutorials/developertools/sourcemaps/)
+// pour nos fichiers transpilés et le bundle final, mais seules
+// certaines garantissent le bon fonctionnement des points
+// d’arrêt dans Chrome.  On utilise ici celle qui, parmi les «bonnes»,
+// est créée le plus vite par Webpack.
+// devtool: '#inline-source-map'
+
+exports.generateSourceMaps = (type = 'cheap-module-source-map') => ({
+  devtool: type,
+})
+
+// Minifie les JS (auto de base) **et les CSS**.  Définir l’option écrase sa
+// valeur par défaut, donc on la restaure côté Terser.
+exports.minifyAll = () => {
+  const TerserPlugin = require('terser-webpack-plugin')
+  const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+  return {
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          parallel: true,
+        }),
+        new OptimizeCSSAssetsPlugin({}),
+      ],
+    },
+  }
+}
+// Passe les images (inline ou non) par imagemin, lequel délègue aux optimiseurs
+// de l’état de l’art par type d’image : mozjpeg pour les JPEG, pngquant pour les PNG
+// (on désactive optipng, moins performant), gifsicle pour les GIF, et svgo pour les SVG.
+// On cale par défaut le facteur de qualité des JPEG à 75, amplement suffisant pour 99,9% des cas.
+//
+// Si vous spritez les images, assurez-vous de faire passer cette optimisation *après* le spriting,
+// pas avant (donc *avant* en termes de pipeline de chargeurs…).
+exports.optimizeImages = (options = {}) => {
+  options = {
+    optipng: { enabled: false },
+    ...options,
+    mozjpeg: { quality: 75, ...(options.mozjpeg || {}) },
+  }
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(?:jpe?g|png|gif|svg)$/,
+          use: [{ loader: 'image-webpack-loader', options }],
+        },
+      ],
+    },
+  }
+}
+
+// ESLint
+// ------
+//
+// Permet d’assurer le *linting* pendant le build, indépendamment de son recours
+// dans l’éditeur / EDI ou en hook de pre-commit Git.  Exclue `node_modules` par
+// défaut.  La configuration est supposée externe (`.eslintrc.json` ou clé
+// `eslintConfig` dans `package.json`).
+exports.lintJS = ({ include, exclude = /node_modules/ } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include,
+        exclude,
+        use: ['eslint-loader'],
+      },
+    ],
+  },
+})
+
+// Purge le dossier de build (`output.path`), ce qui est notamment utile quand les noms de fichiers
+// changent parfois d’un build à l’autre, par exemple en raison de leur hash.
+exports.cleanDist = (paths, options) => {
+  const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+  return { plugins: [new CleanWebpackPlugin(paths, options)] }
+}
 
 // Chargeurs de syntaxe
 // --------------------
+exports.loadFonts = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.(?:woff2?|eot|ttf|otf)$/,
+        include,
+        exclude,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 10000, name: '[sha256:hash:16].[ext]' },
+          },
+        ],
+      },
+    ],
+  },
+})
+
+exports.loadImages = ({ include, exclude, ieSafeSVGs = true } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.(?:jpe?g|png|gif)$/,
+        include,
+        exclude,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 10000, name: '[sha256:hash:16].[ext]' },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        include,
+        exclude,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              iesafe: ieSafeSVGs,
+              limit: 10000,
+              name: '[sha256:hash:16].[ext]',
+              stripdeclarations: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+})
+
 exports.extractCSS = ({ include, exclude, modules } = {}) =>
   extractStyling({ ext: 'css', include, exclude, modules })
 

d71260c340d3f71270c778918276f3ef2b30d07d
Florent Pellegrin <florent.pellegrin@gmail.com>
2021-01-15 01:00:25 +0100

    Update Button with Sass styles

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -2,20 +2,33 @@ import React from 'react'
 import { bool, node, oneOf } from 'prop-types'
 import classnames from 'classnames'
 
-import './button.css'
+import './button.scss'
 
+/**
+ * Represents a button.
+ * @component
+ * @param {color} color - The color of the button.
+ * @param {variant} variant - The variant of the button.
+ * @param {size} size - The size of the button ['small', 'medium', 'large'].
+ * @param {full} full - [Boolean] If true the button is 100% width.
+ */
 const Button = (
   {
     children,
-    variant = 'text',
+    variant = 'contained',
     color = 'primary',
-    size = 'small',
+    size = 'medium',
     full = false,
+    disabled = false,
   },
   props
 ) => {
   return (
-    <button {...props} className={classnames(variant, color, size, { full })}>
+    <button
+      {...props}
+      className={classnames(variant, color, size, { full })}
+      disabled={disabled}
+    >
       {children}
     </button>
   )
@@ -23,10 +36,11 @@ const Button = (
 
 Button.propTypes = {
   children: node.isRequired,
-  variant: oneOf(['contained', 'outlined', 'text']),
+  variant: oneOf(['contained', 'outlined', 'text', 'naked']),
   color: oneOf(['primary', 'secondary']),
   size: oneOf(['small', 'medium', 'large']),
   full: bool,
+  disabled: bool,
 }
 
 export default Button
--- src/components/atoms/Button/Button.stories.js
+++ src/components/atoms/Button/Button.stories.js
@@ -3,29 +3,13 @@ import React from 'react'
 import Button from './Button'
 
 export default {
-  title: 'Atoms/Button',
+  title: 'Atoms',
   component: Button,
 }
 
-const Template = (args) => <Button {...args}>Valider</Button>
+const Template = (args) => <Button {...args} />
 
-export const Variant = Template.bind({})
-Variant.args = {
-  variant: 'text',
-  children: 'Toto',
+export const BasicButton = Template.bind({})
+BasicButton.args = {
+  children: 'Valider',
 }
-
-export const Default = () => <Button>Coucou</Button>
-
-export const Contained = () => <Button variant='contained'>Hello</Button>
-export const Outlined = () => <Button variant='outlined'>Hello</Button>
-export const Secondary = () => (
-  <Button color='secondary' size='large'>
-    Coucou
-  </Button>
-)
-export const Full = () => (
-  <Button variant='contained' color='secondary' full>
-    Hello
-  </Button>
-)
